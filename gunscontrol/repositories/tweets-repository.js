ea8930825b0b0ab7b9690db31d29b63e0e290c43
elaya <elaya@dome9.com>
2019-01-12 13:29:51 +0200

    initial commit

---
+++ gunscontrol/repositories/tweets-repository.js
@@ -0,0 +1,13 @@
+let Tweet = require('./../models/tweetsModel');
+
+exports.getAllTweets = async function () {
+    return Tweet.find()
+        .catch(() => error("err"));
+};
+
+
+exports.findTweetAndUpdateSentiment = async function(tweetId, sentiment){
+      Tweet.findByIdAndUpdate(tweetId, {$set: {sentiment: sentiment}}, {new:true}, function(err, tank) {
+         if (err) console.log(err);
+     })
+};
---
+++ gunscontrol/routes/tweets-route.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let router = express.Router();
+let tweetService = new (require('../services/tweetsService')).TweetService();
+
+router.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    //res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+    // Request methods you  wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type,authorization,Accept');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+        log.info('end options');
+        return res.status(200).send();
+    }
+    next();
+});
+
+
+router.get('/', async function(req, res, next) {
+    try {
+        let tweetsUpdatedWithSentiment = await tweetService.getTweets();
+        res.send(tweetsUpdatedWithSentiment);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+});
+
+
+//also includes update for alert bundles
+router.put('/', async function(req, res, next){
+    try{
+       //let updatedAlert = await  tweetService.getTweets
+        res.send();
+    }catch(err){
+        console.log(err);
+        next(err);
+    }
+});
+
+
+module.exports = router;
